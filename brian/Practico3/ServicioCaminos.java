9b56d7ec6145e192c8d5397416b87609429ef9a5
Brian Freijomil <brianfreijomil@hotmail.com>
2023-05-20 18:00:32 -0300

    servicio caminos

--- brian/Practico3/ServicioCaminos.java
+++ brian/Practico3/ServicioCaminos.java
@@ -1,28 +1,47 @@
 package brian.Practico3;
 
 import java.util.ArrayList;
-import java.util.List;
-
-import brian.Practico3.Grafo;
+import java.util.Iterator;
 
 public class ServicioCaminos {
 
-	private Grafo<?> grafo;
-	private int origen;
-	private int destino;
-	private int lim;
-	
-	// Servicio caminos
-	public ServicioCaminos(Grafo<?> grafo, int origen, int destino, int lim) {
-		this.grafo = grafo;
-		this.origen = origen;
-		this.destino = destino;
-		this.lim = lim;
-	}
+    private Grafo<?> grafo;
+    private ArrayList<ArrayList<Integer>> caminos;
 
-	public List<List<Integer>> caminos() {
-		// Resolver Caminos
-		return new ArrayList<>();
-	}
+    public ServicioCaminos(Grafo<?> grafo) {
+        this.grafo = grafo;
+        this.caminos = new ArrayList<ArrayList<Integer>>();
+    }
 
+    /*
+    Caminos: dado un origen, un destino y un límite “lim” retorna todos los caminos que, partiendo del
+    vértice origen, llega al vértice de destino sin pasar por más de “lim” arcos. Aclaración importante: en
+    un camino no se puede pasar 2 veces por el mismo arco.
+    */
+
+    public ArrayList<ArrayList<Integer>> caminos(Integer origen, Integer destino, Integer lim) {
+        this.caminos.clear();
+        ArrayList<Integer> camino = new ArrayList<Integer>();
+        this.caminos(origen, destino, lim, camino);
+        return this.caminos;
+    }
+    private void caminos(Integer origen,Integer destino, Integer lim, ArrayList<Integer> camino) {
+        camino.add(origen);
+        if(origen != destino) {
+            Iterator<Integer> it = this.grafo.obtenerAdyacentes(origen);
+            while(it.hasNext()) {
+                int k = it.next();
+                if(!camino.contains(k)) {
+                    this.caminos(k, destino, lim, camino);
+                }
+            }
+            camino.remove(camino.size()-1);
+        }
+        else {
+            caminos.add(camino);
+            System.out.println(caminos);
+            camino.remove(camino.size()-1);
+        }
+
+    }
 }
