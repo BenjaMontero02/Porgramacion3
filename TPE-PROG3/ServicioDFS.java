92b2b14c9cb9fa5762252b724b12b08053b20d49
Brian Freijomil <brianfreijomil@hotmail.com>
2023-05-20 21:04:59 -0300

    se agrego carpeta de tpe

---
+++ TPE-PROG3/ServicioDFS.java
@@ -0,0 +1,50 @@
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+
+public class ServicioDFS {
+
+	private Grafo<?> grafo;
+	private HashMap<Integer, String> hashMapaux;
+
+	public ServicioDFS(Grafo<?> grafo) {
+		this.grafo = grafo;
+		this.hashMapaux = new HashMap<>();
+	}
+	
+	public List<Integer> dfsForest() {
+		Iterator<Integer> it = grafo.obtenerVertices();
+		ArrayList<Integer> recorrido = new ArrayList<>();
+		while(it.hasNext()) {
+			Integer value = it.next();
+			this.hashMapaux.put(value, "blanco");
+		}
+		it = grafo.obtenerVertices();
+		while(it.hasNext()) {
+			int k = it.next();
+			if(hashMapaux.get(k) == "blanco") {
+				this.dfsVisit(k,recorrido);
+			}
+		}
+		return recorrido;
+	}
+
+	private void dfsVisit(int k, ArrayList<Integer> arr) {
+		hashMapaux.replace(k, "amarillo");
+		arr.add(k);
+		Iterator<Integer> adyacentes = grafo.obtenerAdyacentes(k);
+		while(adyacentes.hasNext()) {
+			int i = adyacentes.next(); 
+			if(hashMapaux.get(i) == "blanco") {
+				this.dfsVisit(i, arr);
+			}
+			else
+				if (hashMapaux.get(i) == "amarillo")
+				System.out.println("ciclo detectado");	 
+		}
+		hashMapaux.replace(k, "negro");
+	}
+
+}
