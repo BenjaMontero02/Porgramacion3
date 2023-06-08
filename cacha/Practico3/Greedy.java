1bc0109cf24818b210161ae70e2809772966857b
cacha <monterobenja.2018@gmail.com>
2023-06-08 12:41:13 -0300

    te cabio gallo

--- cacha/Practico3/Greedy.java
+++ cacha/Practico3/Greedy.java
@@ -1,8 +1,24 @@
 package cacha.Practico3;
 
+import java.util.ArrayList;
+
 public class Greedy{
+    private static int puntero = 0;
 
+    public ArrayList<Integer> ej1(ArrayList<Integer> c, ArrayList<Integer> solucion, int totalAPagar){
+        while(totalAPagar != 0){
+            //me selecciona del conjunto de candidatos el valor mas grande, dependiendo el puntero
+            int valor = seleccionar(c); 
+            if((totalAPagar - valor) >= 0){
+                solucion.add(valor);
+                totalAPagar = totalAPagar - valor;
+            }else{
+                puntero++;
+            }
+        }
+    }
 
+    
 
     
 }
