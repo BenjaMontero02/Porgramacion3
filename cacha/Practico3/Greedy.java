7f5b4cf0a7eb0c622d826e01efb723c02b0095e8
cacha <monterobenja.2018@gmail.com>
2023-06-08 12:44:17 -0300

    te cabio gallo

--- cacha/Practico3/Greedy.java
+++ cacha/Practico3/Greedy.java
@@ -5,7 +5,8 @@ import java.util.ArrayList;
 public class Greedy{
     private static int puntero = 0;
 
-    public ArrayList<Integer> ej1(ArrayList<Integer> c, ArrayList<Integer> solucion, int totalAPagar){
+    public ArrayList<Integer> ej1(ArrayList<Integer> c, int totalAPagar){
+        ArrayList<Integer> solucion = new ArrayList<Integer>();
         while(totalAPagar != 0){
             //me selecciona del conjunto de candidatos el valor mas grande, dependiendo el puntero
             int valor = seleccionar(c); 
@@ -16,6 +17,8 @@ public class Greedy{
                 puntero++;
             }
         }
+
+        return solucion;
     }
 
     
