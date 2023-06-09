295bfa46ef1a87fc9a3458ff078c3c262078b45f
cacha <monterobenja.2018@gmail.com>
2023-06-08 22:15:40 -0300

    te cabio gallo

--- cacha/Practico3/Greedy.java
+++ cacha/Practico3/Greedy.java
@@ -21,7 +21,46 @@ public class Greedy{
         return solucion;
     }
 
-    
+    public ArrayList<Integer> ej2(ArrayList<Double>fracciones, int peso, ArrayList<Integer>peso){
 
-    
+        for (int j = 0; j < fracciones.size()-1; j++) {
+            fracciones[j] = 0;
+        }
+        int pesoActual = 0;
+
+        while(pesoActual < peso){
+            int i = seleccion(); //selecciona el producto de mayor valor/peso, asumo que no repite indices
+
+            if((pesoActual + peso[i] < W)){
+                fracciones[i] = 0;
+                pesoActual = pesoActual + peso[i];
+            }else{
+                x[i] = (peso - pesoActual) / peso[i];
+                pesoActual = peso;
+            }
+        }
+
+        return x;
+
+    }
+
+    public ArrayList<Actividades> getActDisponibles(ArrayList<Actividades>c){
+        ArrayList<Actividades> solucion = new ArrayList<Actividades>();
+        while(!c.isEmpty()){
+            
+            Actividad actividad = seleccionar(c); //selecciona la primer actividad del conjuntoy la saca
+
+            if(solucion.isEmpty()){
+                solucion.add(actividad);
+            }else{
+                Actividad ultima = solucion.get(solucion.size()-1);
+
+                if(ultima.getFin() <= actividad.getInicio()){
+                    solucion.add(actividad);
+                }
+            }
+        }
+
+        return solucion;
+    }
 }
