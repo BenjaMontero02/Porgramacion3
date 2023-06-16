8de5727f7bbeb8e318848ea4e84b829274818aae
Brian Freijomil <brianfreijomil@hotmail.com>
2023-06-15 21:22:15 -0300

    servicio BFS

---
+++ brian/Parciales.java/brian/Parciales/Parcial2.java
@@ -0,0 +1,20 @@
+import javax.swing.text.html.HTMLDocument.Iterator;
+
+public class Parcial2 {
+
+
+
+
+
+    public ArrayList<Estacion> greedy(int kilometrosLimite, ListaVinculada<> ruta) {
+
+        Iterator<Node> itEstacion = ruta.estacionesRuta(); //obtengo iterador de lista vinculada de estaciones
+
+        while(estacionA.hasNext()) { //mientras tengo estaciones para recorrer itero
+            Node estacion = seleccionar(C); //obtengo estacion de la del conjunto, el metodo de seleccion funciona con un iterador de la lista vinculada y me va dando cada nodo  siguiente
+            Node siguienteEstacion = estacion.getNext(); //obtengo la siguiente estacion en la ruta
+            int distanciaKm = distancia(estacion,siguienteEstacion); //me devuelve la distancia entre estaciones en km (int)
+            if()
+        }
+    }
+}
